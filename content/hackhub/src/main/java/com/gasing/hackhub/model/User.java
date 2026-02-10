8f251c43e0e005650729325cf83a9bdeb85c4332
Andrea Gls <kaisandrea.othmane@studenti.unicam.it>
2026-02-10 20:53:35 +0100

    Implementazione classe User

--- content/hackhub/src/main/java/com/gasing/hackhub/model/User.java
+++ content/hackhub/src/main/java/com/gasing/hackhub/model/User.java
@@ -1,35 +1,33 @@
 package com.gasing.hackhub.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
+import lombok.*;
 import java.util.List;
 
 @Entity
-@Table(name = "users") // "user" è parola riservata in SQL, meglio usare plurale
-@Data // Lombok genera getter, setter, toString, equals, hashcode
+@Data
+@Table(name = "Utente") // user non si può usare in SQL per nominare una tabella
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // id si autoincrementa nel database
+    private long id;
 
-    @Column(nullable = false, unique = true)
-    private String email;
-
-    @Column(nullable = false)
-    private String passwordHash; // Nel diagramma è passwordHash
-
-    @Column(nullable = false)
+    @Column(nullable = false) // non accetta valori vuoti
     private String nome;
 
     @Column(nullable = false)
     private String cognome;
 
-    // Relazione con StaffAssignment (da scommentare quando implementerai l'area Evento&Staff)
-    // @OneToMany(mappedBy = "user")
-    // private List<StaffAssignment> assignments;
+    @Column(nullable = false, unique=true) // unique fa si che non si possa usare l'email per due utenti
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash; // la password non deve esser emostrata in chiaro
+
+    @OneToMany(mappedBy = "user") // serve per il collegamento di un utente al suo ruolo staff che può avere in vari hackathon
+    private List<StaffAssignment> assignments;
+
 }
