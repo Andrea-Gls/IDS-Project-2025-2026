460014f416bc791d54ef03396dbe794c22efc0f4
Ore <ricciuti.alessio@hotmail.com>
2026-02-14 19:44:50 +0100

    feat security auth

--- content/hackhub/src/main/java/com/gasing/hackhub/service/AuthService.java
+++ content/hackhub/src/main/java/com/gasing/hackhub/service/AuthService.java
@@ -1,67 +1,81 @@
 package com.gasing.hackhub.service;
 
-import com.gasing.hackhub.dto.auth.request.RegisterRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.gasing.hackhub.config.JwtService;
 import com.gasing.hackhub.dto.auth.request.LoginRequest;
+import com.gasing.hackhub.dto.auth.request.RegisterRequest;
+import com.gasing.hackhub.dto.auth.response.AuthResponse;
 import com.gasing.hackhub.dto.auth.response.UserResponse;
 import com.gasing.hackhub.model.User;
 import com.gasing.hackhub.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
     @Autowired
     private UserRepository userRepository;
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+    @Autowired
+    private JwtService jwtService;
+    
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-    public UserResponse register(RegisterRequest request) {             // metodo per registrarsi
-
-        // Controllo se l'email esiste già
-        // Se c'è già un utente con questa mail, blocco tutto e lancio un errore.
+    // --- 1. REGISTRAZIONE ---
+    public AuthResponse register(RegisterRequest request) {
+        
         if (userRepository.existsByEmail(request.getEmail())) {
-            throw new RuntimeException("Errore: Email già in uso!");
+            throw new RuntimeException("Email già in uso!");
         }
 
-        // Altrimenti creo l'Entity User dai dati della Request
-        // Passo i dati dal DTO (JSON) all'Entità (Database)
-        User newUser = new User();
-        newUser.setNome(request.getNome());
-        newUser.setCognome(request.getCognome());
-        newUser.setEmail(request.getEmail());
+        User user = new User();
+        user.setNome(request.getNome());
+        user.setCognome(request.getCognome());
+        user.setEmail(request.getEmail());
+        user.setPasswordHash(passwordEncoder.encode(request.getPassword()));
 
-        // Gestione Password
-        // Qui dovrei criptarla (BCrypt) per ora la salviamo così com'è per testare
-        newUser.setPasswordHash(request.getPassword());
+        User savedUser = userRepository.save(user);
 
-        // Salvo nel Database
-        // Il metodo save restituisce l'utente aggiornato con id creato dal DB.
-        User savedUser = userRepository.save(newUser);
+        // Genero il Token e i dati utente
+        String jwtToken = jwtService.generateToken(savedUser);
+        UserResponse userData = mapToUserResponse(savedUser);
 
-        // Converto in DTO di risposta
-        // Non restituisco 'savedUser' perché contiene la password.
-        // Creo un oggetto pulito 'UserResponse' solo con i dati pubblici.
-        return mapToUserResponse(savedUser);
+        // Restituisco la scatola grande che contiene ENTRAMBI
+        return new AuthResponse(jwtToken, userData);
     }
 
-    public UserResponse login(LoginRequest request) {                   // metodo di login
+    // --- 2. LOGIN ---
+    public AuthResponse login(LoginRequest request) {
+        
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
 
-        // Cerco l'utente per Email
-        // Se non lo trovo, lancio un errore generico (per sicurezza non diciamo "email non trovata").
         User user = userRepository.findByEmail(request.getEmail())
-                .orElseThrow(() -> new RuntimeException("Login fallito: Credenziali errate"));
+                .orElseThrow(() -> new RuntimeException("Utente non trovato"));
 
-        // Controllo la Password
-        // Confronto quella inviata con quella nel DB.
-        if (!request.getPassword().equals(user.getPasswordHash())) {
-            throw new RuntimeException("Login fallito: Credenziali errate");
-        }
+        // Genero il Token e i dati utente
+        String jwtToken = jwtService.generateToken(user);
+        UserResponse userData = mapToUserResponse(user);
 
-        // Se tutto ok, restituisco i dati dell'utente (senza password)
-        return mapToUserResponse(user);
+        // Restituisco la scatola grande che contiene ENTRAMBI
+        return new AuthResponse(jwtToken, userData);
     }
 
-    // Metodo privato
-    private UserResponse mapToUserResponse(User user) {    // Serve per non riscrivere il codice di conversione due volte
+    // --- METODO PRIVATO ---
+    private UserResponse mapToUserResponse(User user) {
         UserResponse response = new UserResponse();
         response.setId(user.getId());
         response.setNome(user.getNome());
